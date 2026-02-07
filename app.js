4ad08294d1e055d424730f8ca987249d1440815d
BTD1GHOST <124221535+BTD1GHOST@users.noreply.github.com>
2026-02-06 22:10:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 import {
+  addDoc,
+  onSnapshot,
+  orderBy,
+  limit
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+import {
   getFirestore,
   doc,
   setDoc,
